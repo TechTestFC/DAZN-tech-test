5126b48e818de0f4a591be689dae8271b03bd3d9
Florian Chauvel <florian@nuvo.ai>
2018-09-19 20:41:38 +0200

    fixed broken app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
+import API from './api';
 
 const node = document.getElementById('root');
 
 ReactDOM.render(
-  <App />,
+  <App API={API} />,
   node
 );
